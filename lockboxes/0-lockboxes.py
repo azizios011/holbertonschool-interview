7c09f430bf92efce45d222688b59b1bf62dd882e
azizios011 <hdriaziz699@gmail.com>
2024-01-12 22:47:31 +0100

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-"""Lockboxes"""
-def canUnlockAll(boxes):
-    """Method that determines if all boxes can be opened"""
 
+"""
+Module for the canUnlockAll function
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+    - boxes: A list of lists where each box contains keys to other boxes.
+
+    Returns:
+    - True if all boxes can be opened, else False.
+    """
     for key in range(1, len(boxes)):
         flag = False
         for box in range(len(boxes)):
