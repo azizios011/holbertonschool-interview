b0464260568617cbf069a9a8710a5c8085c657ed
azizios011 <hdriaziz699@gmail.com>
2024-04-05 16:38:30 +0100

    Log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""a script that reads 'stdin' line by line and computes metrics"""
-
+"""
+This script reads lines from stdin, matches them with
+a specific log format, and computes metrics.
+"""
 
 import sys
 import re
 from collections import defaultdict
 
-
-""" Regular expression pattern to match the log format """
-
+# Regular expression pattern to match the log format
 LOG_PATTERN = re.compile(
     r'^(\d+\.\d+\.\d+\.\d+) '
     r'\[(.*?)\] "GET /projects/260 HTTP/1\.1" '
@@ -17,17 +17,22 @@ LOG_PATTERN = re.compile(
 
 
 def print_statistics(total_size, status_counts):
-
-    """Function to print statistics"""
-
+    """
+    Function to print statistics.
+    Args:
+        total_size (int): Total file size.
+        status_counts (dict): Dictionary containing status code counts.
+    """
     print(f"File size: {total_size}")
     for status_code in sorted(status_counts.keys()):
         print(f"{status_code}: {status_counts[status_code]}")
 
 
 def main():
-    """Extract information from the log line and
-    Print statistics after every 10 lines"""
+    """
+    Extract information from the log line and
+    print statistics after every 10 lines.
+    """
     total_size = 0
     status_counts = defaultdict(int)
     line_count = 0
@@ -36,7 +41,6 @@ def main():
         for line in sys.stdin:
             match = re.match(LOG_PATTERN, line.strip())
             if match:
-
                 ip_address, _, status_code, file_size = match.groups()
                 total_size += int(file_size)
                 status_counts[status_code] += 1
