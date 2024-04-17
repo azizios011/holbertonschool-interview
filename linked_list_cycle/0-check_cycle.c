d7d521f46ccb2c61fe2ec169ebf346b87e32ffe3
azizios011 <hdriaziz699@gmail.com>
2024-04-18 00:18:32 +0100

    Linked list cycle

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -9,18 +9,18 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow_ptr = list;
-    listint_t *fast_ptr = list;
+	listint_t *slow_ptr = list;
+	listint_t *fast_ptr = list;
 
-    while (slow_ptr && fast_ptr && fast_ptr->next)
-    {
-        slow_ptr = slow_ptr->next;
-        fast_ptr = fast_ptr->next->next;
+	while (slow_ptr && fast_ptr && fast_ptr->next)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
 
 
-        if (slow_ptr == fast_ptr)
-            return 1;
-    }
+		if (slow_ptr == fast_ptr)
+			return (1);
+	}
 
-    return 0;
+	return (0);
 }
