26696223473118afbfed44eba6010e7ef034b489
azizios011 <hdriaziz699@gmail.com>
2024-04-24 21:17:47 +0100

    UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    a method that determines if a given data
+    set represents a valid UTF-8 encoding.
+"""
+
 
 def validUTF8(data):
     num_bytes = 0
@@ -21,4 +26,3 @@ def validUTF8(data):
             num_bytes -= 1
 
     return num_bytes == 0
-    
