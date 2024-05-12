c675a0d161515f08bd37a0b703497a302fa49fe1
azizios011 <hdriaziz699@gmail.com>
2024-05-12 22:55:47 +0100

    2D Menger sponge

---
+++ menger/0-menger.c
@@ -0,0 +1,12 @@
+#include "menger.h"
+
+/**
+ * menger - a function that draws a 2D Menger Sponge.
+ * @level: is the level of the Menger Sponge to draw.
+ * Returns: 0.
+ * 
+*/
+void menger(int level)
+{
+
+}
