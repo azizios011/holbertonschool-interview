4935de0a35f399c41ac4d93c3f8c41e17aedbab2
azizios011 <hdriaziz699@gmail.com>
2024-06-02 13:20:24 +0100

    Header File

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -5,5 +5,7 @@
 #include <stdlib.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+void print_array(int *array, size_t left, size_t right);
 
 #endif /* HEADER FILE */
