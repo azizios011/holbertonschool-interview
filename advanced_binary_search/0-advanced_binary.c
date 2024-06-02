5af2f6416ed080b7c20c1d309cd958665a8f9cec
azizios011 <hdriaziz699@gmail.com>
2024-06-02 13:08:50 +0100

    Advanced Binary Search

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,5 +1,12 @@
 #include "search_algos.h"
 
+/**
+ * advanced binary - a function that searches for a value in a sorted array of integers.
+ * @array: is a pointer to the first element of the array to search in.
+ * @size: is the number of elements in array.
+ * @value: is the value to search for.
+ * Returns: the index where value is located, or -1 if value is not present in array or if array is NULL.
+*/
 int advanced_binary(int *array, size_t size, int value)
 {
 
