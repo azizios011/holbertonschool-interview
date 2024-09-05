af673a05184ac6c55b7d263a9fcb37cc9b6d3444
azizios011 <hdriaziz699@gmail.com>
2024-09-05 14:12:57 +0100

    Header File

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -1,5 +1,9 @@
-#ifndef HEADER_FILE
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -18,12 +22,18 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
+/* Function Prototypes */
 avl_t *sorted_array_to_avl(int *array, size_t size);
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end);
-void binary_tree_print(const binary_tree_t *tree);
-void free_tree(avl_t *root);
 
-#endif /* HEADER File */
+/* Utility functions */
+avl_t *create_avl_node(avl_t *parent, int value);
+avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent);
+
+/* Function to print the binary tree */
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
