1c719791f90e1de205db3d76ace92e86121e9bcf
azizios011 <hdriaziz699@gmail.com>
2024-09-05 13:58:20 +0100

    UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -18,16 +18,15 @@ def validUTF8(data):
     num_bytes = 0
 
     for num in data:
-        if num > 255:
-            return False
+        num = num & 0xFF
 
         if num_bytes == 0:
             if num >> 7 == 0b0:
                 continue
-            elif num >> 4 == 0b1110:
-                num_bytes = 2
             elif num >> 5 == 0b110:
                 num_bytes = 1
+            elif num >> 4 == 0b1110:
+                num_bytes = 2
             elif num >> 3 == 0b11110:
                 num_bytes = 3
             else:
