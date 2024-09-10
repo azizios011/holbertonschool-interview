423cdc4ce2ae66e87e105c2458d57e4e7fe215c2
azizios011 <hdriaziz699@gmail.com>
2024-09-10 14:41:01 +0100

    Simple RegEx

--- regex/regex.c
+++ regex/regex.c
@@ -10,20 +10,19 @@
 int regex_match(char const *str, char const *pattern)
 {
     /* Base case: if pattern is empty */
-    if (*pattern == '\0')
-        return (*str == '\0');
+	if (*pattern == '\0')
+		return (*str == '\0');
 
     /* Case when the next character in the pattern is '*' */
-    if (*(pattern + 1) == '*')
-    {
-        /* Move on if the pattern after '*' matches, or if current character matches */
-        return (regex_match(str, pattern + 2) ||
-               ((*str != '\0') && (*str == *pattern || *pattern == '.') && regex_match(str + 1, pattern)));
-    }
+	if (*(pattern + 1) == '*')
+	{
+		return (regex_match(str, pattern + 2) ||
+			((*str != '\0') && (*str == *pattern || *pattern == '.') &&
+			regex_match(str + 1, pattern)));
+	}
 
-    /* Case when there's no '*' */
-    if (*str != '\0' && (*str == *pattern || *pattern == '.'))
-        return (regex_match(str + 1, pattern + 1));
+	if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+		return (regex_match(str + 1, pattern + 1));
 
-    return (0);
+	return (0);
 }
